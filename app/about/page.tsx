a5571b6a1529448a9a83b5af70987e0334364153
pavelbeard <pavelborodin1995@gmail.com>
2025-05-08 01:17:26 +0200

    Add About page and integrate ClerkProvider in layout for user authentication

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About Greender</h1>
+      <p>Greender is a platform that allows you to connect with others.</p>
+      <p>We are focused on providing a great user experience.</p>
+      <p>Our goal is to help you find the right connections.</p>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+
 export default function Home() {
-  return (
-    <main>
+  const { isSignedIn, isLoaded, user } = useUser();
+  return <main>
+    {isSignedIn ? (
       <div>
-        <h1>Hello, I&apos;m Greender</h1>
-        <p>Welcome to my page!</p>
+      <h1>Hello, I&apos;m Greender</h1>
+      <p>Welcome to my page!</p>
+    </div>
+    ) : (
+      <div>
+        <h1>Welcome to Greender</h1>
+        <p>Please sign in to see more content.</p>
       </div>
-    </main>
-  );
+    )}
+  </main>;
 }
