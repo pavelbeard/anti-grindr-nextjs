24ae8ff7bd9282ff4301f0e6a624ed4a36337df8
pavelbeard <pavelborodin1995@gmail.com>
2025-05-09 17:31:51 +0200

    Comment out eslint-config-prettier in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  eslintConfigPrettier,
+  // eslintConfigPrettier,
   eslintPluginPrettier,
 ];
 
