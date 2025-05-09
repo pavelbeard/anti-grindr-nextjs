b8bf87ad4818da12136485aaea98a08f89ca0f4d
pavelbeard <pavelborodin1995@gmail.com>
2025-05-09 17:32:23 +0200

    refactor: enhance Home component layout and styling with background images

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
+import GreenderBackground from "@/app/assets/background.png";
+import GreenderMainBackground from "@/app/assets/muscular_men_hugging.png";
 
 export default function Home() {
   const { isSignedIn, isLoaded, user } = useUser();
-  return <main>
-    {isSignedIn ? (
-      <div>
-      <h1>Hello, I&apos;m Greender</h1>
-      <p>Welcome to my page!</p>
-    </div>
-    ) : (
-      <div>
-        <h1>Welcome to Greender</h1>
-        <p>Please sign in to see more content.</p>
-      </div>
-    )}
-  </main>;
+  return (
+    <main
+      className="min-h-screen bg-[#E8B94E]"
+      style={{
+        backgroundImage: `url(${GreenderMainBackground.src})`,
+        backgroundSize: "cover",
+        backgroundPositionX: "0",
+      }}
+    >
+      {isSignedIn ? (
+        <div>
+          <h1>Hello, I&apos;m Greender</h1>
+          <p>Welcome to my page!</p>
+        </div>
+      ) : (
+        <div>
+          <h1>Welcome to Greender</h1>
+          <p>Please sign in to see more content.</p>
+        </div>
+      )}
+    </main>
+  );
 }
