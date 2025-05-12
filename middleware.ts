a337d2c7934680674cf66075def0706bd620da59
pavelbeard <pavelborodin1995@gmail.com>
2025-05-13 01:22:09 +0200

    feat: add sign-up route to public route matcher in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/about", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/about",
+  "/",
+]);
 
 // Make sure that the `/api/webhooks/(.*)` route is not protected here
 export default clerkMiddleware(async (auth, req) => {
