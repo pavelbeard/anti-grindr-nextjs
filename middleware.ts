51565b857052b7aea637bfaa435e995453d49860
pavelbeard <pavelborodin1995@gmail.com>
2025-05-13 11:38:19 +0200

    feat: refactor middleware to define AUTHORIZED_PARTIES and improve code structure

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+const AUTHORIZED_PARTIES =
+  process.env.NODE_ENV == "production"
+    ? ["https://greender.com"]
+    : ["http://localhost:3000"];
+
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
@@ -8,11 +13,16 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 // Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+export default clerkMiddleware(
+  async (auth, req) => {
+    if (!isPublicRoute(req)) {
+      await auth.protect();
+    }
+  },
+  {
+    authorizedParties: AUTHORIZED_PARTIES,
   }
-});
+);
 
 export const config = {
   matcher: [
