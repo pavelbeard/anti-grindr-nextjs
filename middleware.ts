dd2b66bcd445f13b942c3323b53bf6d40292db39
pavelbeard <pavelborodin1995@gmail.com>
2025-05-13 17:14:39 +0200

    Reset to previous commit due cors is not working.

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextFetchEvent, NextRequest } from "next/server";
 
 const AUTHORIZED_PARTIES =
   process.env.NODE_ENV == "production"
@@ -13,41 +12,17 @@ const isPublicRoute = createRouteMatcher([
   "/",
 ]);
 
-export default function middleware(req: NextRequest, event: NextFetchEvent) {
-  if (req.method === "OPTIONS") {
-    const origin = req.headers.get("origin");
-    return new Response(null, {
-      status: 204,
-      headers: {
-        "Access-Control-Allow-Origin": origin || "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":
-          "Content-Type, Authorization,  Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Max-Age": "86400",
-      },
-    });
-  }
-
-  const auth = clerkMiddleware(async (auth, req) => {
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware(
+  async (auth, req) => {
     if (!isPublicRoute(req)) {
       await auth.protect();
     }
-  });
-
-  return auth(req, event);
-}
-
-// export default clerkMiddleware(
-//   async (auth, req) => {
-//     if (!isPublicRoute(req)) {
-//       await auth.protect();
-//     }
-//   },
-//   {
-//     authorizedParties: AUTHORIZED_PARTIES,
-//   }
-// );
+  },
+  {
+    authorizedParties: AUTHORIZED_PARTIES,
+  }
+);
 
 export const config = {
   matcher: [
