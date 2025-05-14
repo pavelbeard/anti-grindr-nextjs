fdb0ec172386785847d22b1fbb4767c976a0e528
pavelbeard <pavelborodin1995@gmail.com>
2025-05-14 11:30:55 +0200

    feat: AUTHORIZED_PARTIES now has preview ("test") environment

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const AUTHORIZED_PARTIES =
-  process.env.NODE_ENV == "production"
+  process.env.NODE_ENV === "production"
     ? ["https://greender.com"]
-    : ["http://localhost:3000"];
+    : process.env.NODE_ENV === "test"
+      ? [] // For preview environment with no fixed domain
+      : ["http://localhost:3000"]; // Default for development
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
