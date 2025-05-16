c35415f08c6321f9b406dbadb7abf7cbc7b77f29
pavelbeard <pavelborodin1995@gmail.com>
2025-05-16 18:17:43 +0200

    Implement structural updates and optimizations across multiple modules

---
+++ app/(web)/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex flex-col bg-[var(--bg-image-public)]">
+      {children}
+    </main>
+  );
+}
