c60776cc4500b41d5876eeea41c70f6ec21779f1
pavelbeard <pavelborodin1995@gmail.com>
2025-05-21 21:26:43 +0200

    fix: comment out unused resolver and initial values in useAddImage hook

--- lib/hooks/settings/useAddImage.ts
+++ lib/hooks/settings/useAddImage.ts
@@ -1,11 +1,10 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 export default function useAddImage() {
   const form = useForm({
-    resolver: zodResolver(AddImageSchema),
-    initialValues: {
-      file: null,
-    },
+    // resolver: zodResolver(AddImageSchema),
+    // initialValues: {
+    // file: null,
+    // },
   });
 }
