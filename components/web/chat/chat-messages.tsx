9a59455f740c5d6cff0fbd3146cc3cf03a71a7ff
pavelbeard <pavelborodin1995@gmail.com>
2025-06-08 12:03:13 +0200

    feat(chat): refactor ChatMessages component to improve message rendering and styling

--- components/web/chat/chat-messages.tsx
+++ components/web/chat/chat-messages.tsx
@@ -1,16 +1,40 @@
 import { useChatContainerContext } from "@/lib/providers/chat-container-context";
+import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/nextjs";
 import React from "react";
 
-
 export default function ChatMessages() {
-  const { allMessages} = useChatContainerContext();
+  const { user } = useUser();
+  const { allMessages } = useChatContainerContext();
   return (
-    <ul className="flex-1 overflow-y-auto p-4">
+    <div className="flex-1 overflow-y-auto p-4 flex flex-col gap-y-2">
       {allMessages.map((message, index) => (
-        <li key={index}>
-          <strong>{message.userId}</strong>: {message.text}
-        </li>
+        <div
+          key={index}
+          className={cn(
+            "flex flex-col",
+            message.userId === user?.id ? "items-start" : "items-end"
+          )}
+        >
+          <span
+            className={cn(
+              "font-semibold px-4 py-2 ",
+              message.userId === user?.id
+                ? "bg-blue-500 rounded-t-lg rounded-br-lg"
+                : "bg-green-500 rounded-t-lg rounded-bl-lg"
+            )}
+          >
+            {message.text}
+          </span>
+          <span className="text-xs text-gray-500">
+            {/* INTERNALIZATION WILL COME SOON */}
+            {new Date(message.createdAt).toLocaleTimeString("es-ES", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </span>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
